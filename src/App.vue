5348e2811a11ffb4a2b001bdfa7a21241a1447ef
Gilbert <gilbert@UFISs-MacBook-Pro.local>
2022-11-24 19:31:05 -0700

    navigation done

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,14 @@
       <base-card class="py-3">
       <template #buttons>
         <v-card-actions>
-        <v-btn color="purple" rounded="0">Stored Resources</v-btn>
-        <v-btn color="purple" class="bg-purple" variant="flat" rounded="0">Add resource</v-btn>
+        <v-btn color="purple" rounded="0" :variant="setStoredResource" @click="changeComponent('stored-resource')" >Stored Resources</v-btn>
+        <v-btn color="purple" :variant="setAddResource" rounded="0" @click="changeComponent('add-resource')">Add resource</v-btn>
       </v-card-actions>
       </template>
       </base-card>
-      <add-resource></add-resource>
-      
+      <keep-alive>
+      <component :is="activeComp"></component>
+    </keep-alive>
     </v-main>
   </v-app>
 </template>
@@ -20,13 +21,32 @@
 import TheHeader from './components/TheHeader.vue'
 import BaseCard from './components/BaseCard.vue';
 import AddResource from './components/AddResource.vue';
+import StoredResource from './components/StoredResource.vue';
 
 
 export default {
-  components: { TheHeader,BaseCard, AddResource},
+  components: {
+    TheHeader,
+    BaseCard, 
+    AddResource, 
+    StoredResource
+  },
   data(){
     return {
-      on: false
+      activeComp: 'stored-resource'
+    }
+  },
+  methods: {
+      changeComponent(cmp){
+        this.activeComp = cmp
+      }
+  },
+  computed: {
+    setAddResource(){
+      return this.activeComp == 'add-resource' ? 'flat' : 'text'
+    },
+    setStoredResource(){
+      return this.activeComp == 'stored-resource' ? 'flat' : 'text' 
     }
   }
 }
